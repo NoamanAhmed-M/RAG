c4f6fc706b8c579094c8ac98e6b985b54cdb19fd
NoamanAhmed-M <77328177+NoamanAhmed-M@users.noreply.github.com>
2026-02-12 14:18:58 +0100

    Delete v1.0 directory

--- v1.0/v1.0/embedding_functions.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.embeddings import OllamaEmbeddings
-
-def get_embedding_function():
-    return OllamaEmbeddings(
-        model="mistral:latest",
-        base_url="http://localhost:11434"
-    )
